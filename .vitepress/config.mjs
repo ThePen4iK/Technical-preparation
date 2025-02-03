d56538d3be313441ed201c6fb90cb49ee8136546
CloudBj <cloudbj1102@gmail.com>
2025-02-03 10:57:47 +0200

    add vercel.json

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "Technical preparation",
   description: "A VitePress Site",
+  base: '/',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     // search: {
