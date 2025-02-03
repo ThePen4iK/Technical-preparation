0415dcb4f7e2a2cbe69f2337f1b7e3c9ad1d212e
CloudBj <cloudbj1102@gmail.com>
2025-02-03 10:40:37 +0200

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,41 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Technical preparation",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // search: {
+    //   provider: 'local'
+    // },
+    //в шапке навигация
+    nav: [
+      { text: 'Home', link: '/' },
+      // { text: 'Examples', link: '/markdown-examples' }
+    ],
+
+    sidebar: [
+      {
+        text: 'JavaScript',
+        items: [
+          { text: 'JS', link: '/JavaScript/javascript' },
+          // { text: 'Markdown Examples', link: '/markdown-examples' },
+          // { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      },
+      {
+        text: 'Markdown',
+        items: [
+          { text: 'CSS', link: '/Markdown/css' },
+          // { text: 'Markdown Examples', link: '/markdown-examples' },
+          // { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    // socialLinks: [
+    //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    // ]
+  }
+})
